fa5f17d27182c0ec650750bac0c1e8433dd4c4a6
Aniket_The_TechWhiz <creatorpanda26@gmail.com>
2024-12-19 08:18:22 +0530

    update

---
+++ loop_inPython/for_loop.py
@@ -0,0 +1,24 @@
+l=[1,2,4,3,6,7]
+for i in l:
+    print(i) 
+
+str="aniket"
+for i in str:
+    print(i)
+else :
+    print("End")
+
+t=(1,2,3,4,9,6,7)
+num=9
+for i in t:
+    if(num==i):
+        print("found")
+    i+=1
+
+#range
+
+'''
+for i in range (5)      #(stop)
+for i in range (1,5)      #(start,stop)
+for i in range (1,5,2)      #(start,stop,step)
+'''
